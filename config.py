ab56cb6f5396ed70a4f8117188916c28790f0043
W-Y999 <Amara.Yuki06@gmail.com>
2025-11-29 11:02:40 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
+class Config:
+    """项目的超参数配置（可选）"""
+    
+    embedding_dim = 128
+    gru_hidden_dim = 128
+    cnn_filters = 64
+    learning_rate = 1e-3
+    batch_size = 32
+    max_seq_len = 128
+    
+    # TODO: add more configs as needed
 
